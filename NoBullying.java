4a2c56f1bc09ee8ddd54466bc8f9798f3a36909e
LUIS CARLOS BATISTA DE SOUZA <396301368@alunos.etec.itaqua>
2019-03-14 20:36:14 -0300

    Nono Passo

--- NoBullying.java
+++ NoBullying.java
@@ -9,6 +9,8 @@ public static void main(String[] args){
 	System.out.println("Passo 6: A educação é a ferramenta mais eficiente contra o bullying, ela nos torna pessoas melhores.");
 	System.out.println("Passo 7: Não incentive brincadeiras ou apelidos de mal gosto.");
 	System.out.println("Passo 8: Diminuir outras pessoas não vai te fazer uma pessoa melhor.");
+	System.out.println("Passo 9: Realize campanhas contra a bullying.");
+
 
 
 
