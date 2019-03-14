168cfc5506c74e9e233f4ae28677ad1d7dbb7628
LUIS CARLOS BATISTA DE SOUZA <396301368@alunos.etec.itaqua>
2019-03-14 20:16:48 -0300

    Quinto Passo

--- NoBullying.java
+++ NoBullying.java
@@ -5,5 +5,7 @@ public static void main(String[] args){
 	System.out.println("Passo 2: Não caçoe de outras pessoas , trate-as do mesmo jeito que você gostaria de ser tratado.");
 	System.out.println("Passo 3: Saiba lidar com esse tipo de situação, nem sempre encontraremos pessoas agradavéis na nossa vida.");
 	System.out.println("Passo 4: Conscientize pessoas que a prática de bullying afeta e destrói vidas.");
+	System.out.println("Passo 5: A melhor forma de conscientização, vem através de palestras.");
+
 } 
 } 
