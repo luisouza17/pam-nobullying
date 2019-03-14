22c74faad2182a8606f789c53452a70e148c5470
LUIS CARLOS BATISTA DE SOUZA <396301368@alunos.etec.itaqua>
2019-03-14 20:03:14 -0300

    Segundo Passo

--- NoBullying.java
+++ NoBullying.java
@@ -2,6 +2,6 @@ public class NoBullying {
 public static void main(String[] args){
 	System.out.println('Programa NoBullying');
 	System.out.println("Passo 1: Ajude aquela pessoa próxima a você que parece estar triste.");
-	
+	System.out.println("Passo 2: Não caçoe de outras pessoas , trate-as do mesmo jeito que você gostaria de ser tratado.");
 } 
 } 
