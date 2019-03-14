5318662de9bcbc7254b58a8937809307fc6d05d2
LUIS CARLOS BATISTA DE SOUZA <396301368@alunos.etec.itaqua>
2019-03-14 20:13:14 -0300

    Quarto Passo

--- NoBullying.java
+++ NoBullying.java
@@ -4,5 +4,6 @@ public static void main(String[] args){
 	System.out.println("Passo 1: Ajude aquela pessoa próxima a você que parece estar triste.");
 	System.out.println("Passo 2: Não caçoe de outras pessoas , trate-as do mesmo jeito que você gostaria de ser tratado.");
 	System.out.println("Passo 3: Saiba lidar com esse tipo de situação, nem sempre encontraremos pessoas agradavéis na nossa vida.");
+	System.out.println("Passo 4: Conscientize pessoas que a prática de bullying afeta e destrói vidas.");
 } 
 } 
